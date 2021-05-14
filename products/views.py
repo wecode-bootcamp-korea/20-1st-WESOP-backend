3f35a20bd44eb4c2b569631d2f1d4981e6a50488
hwaya2828 <hwaya2828@gmail.com>
2021-05-14 20:36:12 +0900

    REFACTOR: 리뷰 사항 반영 - List comprehension 시도 2

--- products/views.py
+++ products/views.py
@@ -25,40 +25,29 @@ class DetailProductView(View):
         
         features = product.feature.all()
         feature_category_id = set([feature.feature_category_id for feature in features])
-
         feature_result = []
         for id in feature_category_id:
             feature_category_name = FeatureCategory.objects.get(id=id).name
-            feature_detail_result = []
-            for i in features.filter(feature_category_id=id):
-                feature_detail_result.append(i.name)
+            feature_detail_result = [i.name for i in features.filter(feature_category_id=id)]
             feature_result.append(
                 {
                     "feature_category_name" : feature_category_name,
                     "features" : feature_detail_result
                 }
             )
-
         results["feature"] = feature_result
 
         ingredients = product.ingredient.all()
-        ingredient_result = [ingredient.name for ingredient in ingredients]
-            
+        ingredient_result = [ingredient.name for ingredient in ingredients]            
         results["ingredient"] = ingredient_result
        
         product_selections = ProductSelection.objects.filter(product_id=product.id)
-        product_selection_result = []
-
-        for product_selection in product_selections:
-            product_selection_result.append(
-                {
-                    "product_name" : product.name,
-                    "size" : product_selection.size,
-                    "price" : product_selection.price,
-                    "image_url" : product_selection.image_url
-                }
-            )
-
+        product_selection_result = [{
+                                "product_name" : product.name,
+                                "size" : product_selection.size,
+                                "price" : product_selection.price,
+                                "image_url" : product_selection.image_url
+                            } for product_selection in product_selections]
         results["product_selection"] = product_selection_result
 
         return JsonResponse({'result': results}, status=200)
