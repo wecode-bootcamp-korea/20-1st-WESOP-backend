9f2b3cbbe6328f83a22d54a95459c96907e63e01
hwaya2828 <hwaya2828@gmail.com>
2021-05-15 10:43:20 +0900

    REFACTOR: 리뷰 사항 반영 - List comprehension 시도 2

--- products/views.py
+++ products/views.py
@@ -14,16 +14,16 @@ class OpenCategoryView(View):
             results  = self.ShowCategory(category)
         else:
             return JsonResponse({'MESSAGE':'INVALID_PATH'}, status=404)
-        return JsonResponse({'MESSAGE':results}, status=200)
+        return JsonResponse({'result':results}, status=200)
 
     def ShowCategory(self, category):
         products = Product.objects.filter(category_id=category.id)
         results  = [{
-                "menu_name"         : Menu.objects.get(id=category.menu_id).name,
-                "category_name"     : category.name,
-                "description_title" : category.description_title if category.description_title else None,
-                "description"       : category.description if category.description else None
-                }]
+                        "menu_name"         : Menu.objects.get(id=category.menu_id).name,
+                        "category_name"     : category.name,
+                        "description_title" : category.description_title if category.description_title else None,
+                        "description"       : category.description if category.description else None
+                    }]
 
         for product in products:
             product_name = product.name
@@ -43,16 +43,16 @@ class OpenCategoryView(View):
                         "features"              : feature_detail_result
                     }
                 )
-        
+
             ingredients       = product.ingredient.all()
             ingredient_result = [ingredient.name for ingredient in ingredients]
 
             product_selections       = ProductSelection.objects.filter(product_id=product.id)
             product_selection_result = [{
-                                    "size"      : product_selection.size,
-                                    "price"     : product_selection.price,
-                                    "image_url" : product_selection.image_url
-                                    } for product_selection in product_selections]
+                                            "size"      : product_selection.size,
+                                            "price"     : product_selection.price,
+                                            "image_url" : product_selection.image_url
+                                        } for product_selection in product_selections]
 
             results.append(
                 {
