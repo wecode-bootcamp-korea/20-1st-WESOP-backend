818d052836a0334388d4451dbe2d5b6c094ac5a8
hwaya2828 <hwaya2828@gmail.com>
2021-05-15 10:45:54 +0900

    REFACTOR: 리뷰 사항 반영 - List comprehension 시도 4

--- products/views.py
+++ products/views.py
@@ -15,17 +15,17 @@ class DetailProductView(View):
         # product.save() 프로젝트 완성되면 카운트 쌓을 예정입니다 :)
 
         results = {
-                "menu_name"         : menu.name,
-                "category_name"     : category.name,
-                "product_name"      : product.name,
-                "description"       : product.description,
-                "content"           : product.content,
-                "content_image_url" : product.content_image_url
+                    "menu_name"         : menu.name,
+                    "category_name"     : category.name,
+                    "product_name"      : product.name,
+                    "description"       : product.description,
+                    "content"           : product.content,
+                    "content_image_url" : product.content_image_url
                 }
-        
+
         features            = product.feature.all()
-        feature_category_id = set([feature.feature_category_id for feature in features])        
-        feature_result      = []        
+        feature_category_id = set([feature.feature_category_id for feature in features])
+        feature_result      = []
         for id in feature_category_id:
             feature_category_name = FeatureCategory.objects.get(id=id).name
             feature_detail_result = [i.name for i in features.filter(feature_category_id=id)]
@@ -34,20 +34,19 @@ class DetailProductView(View):
                     "feature_category_name" : feature_category_name,
                     "features" : feature_detail_result
                 }
-                )    
+            )
         results["feature"]    = feature_result
 
         ingredients           = product.ingredient.all()
-        ingredient_result     = [ingredient.name for ingredient in ingredients]            
+        ingredient_result     = [ingredient.name for ingredient in ingredients]
         results["ingredient"] = ingredient_result
-       
+
         product_selections          = ProductSelection.objects.filter(product_id=product.id)
         product_selection_result    = [{
-                                    "product_name" : product.name,
-                                    "size"         : product_selection.size,
-                                    "price"        : product_selection.price,
-                                    "image_url"    : product_selection.image_url
-                                    } for product_selection in product_selections]
+                                            "size"      : product_selection.size,
+                                            "price"     : product_selection.price,
+                                            "image_url" : product_selection.image_url
+                                        } for product_selection in product_selections]
         results["product_selection"] = product_selection_result
 
         return JsonResponse({'result': results}, status=200)
