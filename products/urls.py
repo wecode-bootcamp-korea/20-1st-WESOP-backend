805c1a7de0a83665c914c6d831016ba79fb234ae
hwaya2828 <hwaya2828@gmail.com>
2021-05-17 09:18:42 +0900

    REFACTOR: RESTful API 반영하여 URL 수정

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from products.views import DetailProductView
 
 urlpatterns = [
-    path('/detailproduct', DetailProductView.as_view())
+    path('/product', DetailProductView.as_view())
 ]
